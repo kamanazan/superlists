c9ca9c9b2a1a2fa3fad5cccc3151e960ea30a50d
kamanazan <fauzan.rosyadi@gmail.com>
2014-11-27 15:53:24 +0700

    functional tests works properly now

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -8,7 +8,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def setUpClass(cls):
         for arg in sys.argv:
             if '--liveserver' in arg:
-                cls.live_server_url = 'http://' + arg.split('=')[1]
+                cls.server_url = 'http://' + arg.split('=')[1]
 
                 return
         super(StaticLiveServerTestCase,cls).setUpClass()
@@ -34,7 +34,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app.
         # she goes to check out its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do',self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -82,7 +82,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
 
         # Francis visits the home page. There is no sign of Edith's list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -108,7 +108,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         #self.fail('Finish the Test!')
     def test_layout_and_styling(self):
         # Edith goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024,768)
 
         # She notices the input box is nicely centered
