ad7647f4af327307d087f41d1ea2bad1cc878f33
kamanazan <fauzan.rosyadi@gmail.com>
2014-12-02 16:13:39 +0700

    callibrate deploy_tools

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -3,10 +3,10 @@ from fabric.api import env, local, run
 import random
 
 
-REPO_URL = 'https://github.com/hjwp/book-example.git'
+REPO_URL = 'https://github.com/kamanazan/superlists.git'
 
 def deploy():
-    site_folder = '/home/%s/sites/%s' % (env.user, env.host)
+    site_folder = '/home/%s/%s' % (env.user, env.host)
     source_folder = site_folder + '/source'
     _create_directory_structure_if_necessary(site_folder)
     _get_latest_source(source_folder)
